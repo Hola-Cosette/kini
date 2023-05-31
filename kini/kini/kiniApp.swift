c488b8d14f151e770f82c3a93a68f83bee741599
jaelyung <jaekim7724@gmail.com>
2023-05-31 15:23:00 +0900

    [SETTING] : 프로젝트 생성

---
+++ kini/kini/kiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  kiniApp.swift
+//  kini
+//
+//  Created by jaelyung kim on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct kiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
