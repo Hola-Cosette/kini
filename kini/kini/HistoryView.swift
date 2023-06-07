04b3bd048773d503da406b490ed73aea907f49d2
oceanooooo <funboard6287@gmail.com>
2023-06-07 09:38:42 +0900

    [CORRECT] HI-Fi 기준으로 수정 - color, padding, size, 배치 수정
    - [ ] 에셋 추가 안함
    - [ ] 버튼 동작 없음
    - [ ] 차트 반영 없음

--- kini/kini/HistoryView.swift
+++ kini/kini/HistoryView.swift
@@ -8,94 +8,174 @@
 import SwiftUI
 
 struct HistoryView: View {
-    @State var time: [String] = ["아침", "점심", "저녁"]
+    // Variable List
+    var todayDate: String = "YYYY.MM.DD"    // !Sample!
+    var historyDate: String = "MM.DD (W)"   // !Sample!
+    let userName: String = "니코" // !Sample!
+    var mealTime: String = "아침" // !Sample!
+    
+    // Color Extension List
+    let colorBackground = Color(red: 255/255, green: 246/255, blue: 231/255)
+    let colorNavy = Color(red: 34/255, green: 49/255, blue: 116/255)    // Font, Frame Color
+    let colorLightGray = Color(red: 249/255, green: 249/255, blue: 249/255) // Star Score
+    let colorGray = Color(red: 60/255, green: 60/255, blue: 67/255, opacity: 60/100)    // Nutrition Type Text
+    let colorShadow = Color(red: 153/255, green: 123/255, blue: 52/255, opacity: 40/100)    // Shadow Color
+    let colorPre = Color(red: 251/255, green: 227/255, blue: 170/255)
+    let colorPro = Color(red: 251/255, green: 192/255, blue: 54/255)
+    let nutritionColor: [Color] = [Color(red: 254/255, green: 226/255, blue: 125/255),
+                                   Color(red: 237/255, green: 175/255, blue: 199/255),
+                                   Color(red: 190/255, green: 210/255, blue: 125/255),
+                                   Color(red: 240/255, green: 100/255, blue: 51/255),
+                                   Color(red: 127/255, green: 177/255, blue: 225/255),
+                                   Color(red: 216/255, green: 216/255, blue: 216/255)]
+    
+    let time: [String] = ["아침", "점심", "저녁"]
+    let nutritionType: [String] = ["곡류", "고기, 생선, 달걀, 콩류", "채소류",
+                                   "과일류", "우유, 유제품류", "유지, 당류"]
     
     var body: some View {
         ZStack {
-            Text("나의 끼니 기록")
-                .font(.system(size: 20))
-                .position(x: 133+118/2, y: 60+22/2)
-            VStack {
-                Spacer()
-                Rectangle() // Background
-                    .cornerRadius(10)
-                    .foregroundColor(Color.gray)
-                    .frame(width: 371, height: 660)
-                    .overlay {
-                        VStack(spacing: 0) {    // Date - Grade - Chart
-                            Text("(Date)")  // Date
-                                .font(.system(size: 22, weight: .bold))
-                                .frame(height: 25)
-                                .padding(.top, 41)
-                            
-                            Spacer()
-                            
-                            Text("오늘의 영양점수")
-                                .font(.system(size: 15, weight: .bold))
-                                .frame(height: 41)
-                            
-                            HStack {    // Grade
-                                ForEach(time, id: \.self) { time in
-                                    Rectangle()
-                                        .cornerRadius(10)
-                                        .frame(width: 111, height: 148)
-                                        .foregroundColor(.white)
-                                        .overlay {
-                                            VStack(spacing: 0) {    // Grade Image - Time
-                                                Image(systemName: "face.smiling")   // Grade Image
-                                                    .resizable()
-                                                    .scaledToFit()
-                                                    .frame(width: 86, height: 85)
-                                                    .padding(.top, 17)
-                                                
-                                                Text(time)
-                                                    .font(.system(size: 15))
-                                                    .frame(height: 41)
-                                                    .padding(.top, 5)
-                                                
-                                                Spacer()
-                                            }
-                                        }
-                                }   // ~ForEach
-                            }   // ~HStack(Grade)
-                            
-                            Text("오늘의 영양차트")
-                                .font(.system(size: 15, weight: .bold))
-                                .frame(height: 41)
-                                .padding(.top, 14)
-                            
-                            Rectangle() // Chart
-                                .cornerRadius(10)
-                                .foregroundColor(.white)
-                                .frame(width: 343, height: 305)
-                                .padding(.bottom, 18)
-                        }   // ~VStack
-                    }   // ~overlay(Background)
-                Spacer().frame(height: 35)
-            }   // ~VStack
+            colorBackground.ignoresSafeArea()
             
-            VStack {    // Arrow - Sticker - Arrow
-                Spacer()
-                
-                HStack(spacing: 0) {    // Sticker
-                    Image(systemName: "arrowtriangle.left") // prev
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 26, height: 26)
-                        .padding(.trailing, 60)
+            Text("나의 끼니 기록")    // Navigation Title
+                .font(.system(size: 22, weight: .bold))
+                .foregroundColor(colorNavy)
+                .padding(.top, 62)
+                .padding(.bottom, 754)
+            
+            Image(systemName: "chevron.left")    // Navigation BackButton
+//                .font(.system(size: 24)
+                .frame(width: 15, height: 23)
+                .foregroundColor(colorNavy)
+                .padding(EdgeInsets(top: 64, leading: 24, bottom: 757, trailing: 351))
+            
+            VStack {
+                ZStack {    // Nutrition Score
+                    Rectangle() // Background
+                        .cornerRadius(15)
+                        .foregroundColor(.white)
+                        .shadow(color: colorShadow, radius: 4, x: 0, y: 2)
+                        .frame(width: 350, height: 254)
+                        .padding(.top, 140)
                     
-                    Circle()    // Sticker
-                        .frame(width: 80)
-                    
-                    Image(systemName: "arrowtriangle.right") // next
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 26, height: 26)
-                        .padding(.leading, 60)
-                }   // ~HStack
+                    VStack(spacing: 0) {    // Date - Grade - Chart
+                        Text(historyDate)  // Date
+                            .font(.system(size: 12))
+                            .foregroundColor(colorNavy)
+                            .padding(.top, 50)
+                        
+                        Text("오늘의 영양점수")
+                            .font(.system(size: 17, weight: .semibold))
+                            .foregroundColor(colorNavy)
+                            .padding(.top, 5)
+                        
+                        HStack {    // Grade
+                            ForEach(time, id: \.self) { time in
+                                Rectangle()
+                                    .cornerRadius(10)
+                                    .frame(width: 106.07, height: 135)
+                                    .foregroundColor(colorLightGray)
+                                    .overlay {
+                                        VStack(spacing: 0) {    // Grade Image - Time
+                                            Image(systemName: "star.square")   // Grade Image
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 87, height: 84)
+                                                .padding(.top, 8)
+                                            
+                                            Text(time)
+                                                .font(.system(size: 13, weight: .semibold))
+                                                .foregroundColor(colorNavy)
+                                                .padding(.top, 16)
+                                                .padding(.bottom, 9)
+                                        }
+                                    }
+                            }   // ~ForEach
+                        }   // ~HStack(Grade)
+                        .padding(.top, 10)
+                    }   // ~VStack
+                    .padding(.top, 140)
+                }   // ~ZStack(Score)
                 
-                Spacer().frame(height: 654)
-            }
+                ZStack {    // Nutrition Chart
+                    Rectangle() // Background
+                        .cornerRadius(15)
+                        .foregroundColor(.white)
+                        .shadow(color: colorShadow, radius: 4, x: 0, y: 2)
+                        .frame(width: 350, height: 387)
+                        .padding(.top, 17)
+                    
+                    VStack {
+                        Text("오늘의 영양차트")
+                            .font(.system(size: 17, weight: .semibold))
+                            .foregroundColor(colorNavy)
+                            .padding(.top, 15)
+                        
+                        Circle() // Chart
+                            .foregroundColor(colorNavy)
+                            .frame(width: 254, height: 254)
+                            .padding(.bottom, 14)
+                        
+                        HStack(spacing: 0) {    // Nutrition Item 1/2 (Color - Type)
+                            ForEach(0..<2) {
+                                Circle()    // Nutrition Color
+                                    .frame(width: 15, height: 15)
+                                    .foregroundColor(nutritionColor[$0])
+                                    .padding(.trailing, 4)
+                                
+                                Text(nutritionType[$0]) // Nutrition Type
+                                    .font(.system(size: 12))
+                                    .foregroundColor(colorGray)
+                                    .frame(minWidth: 32, alignment: .leading)
+                                    .padding(.trailing, 8)
+                            }
+                        }   // ~HStack(Nutrition Item 1/2)
+                        .padding(.leading, 15)
+                        .padding(.trailing, 153)
+                        
+                        HStack(spacing: 0) {    // Nutrition Item 2/2 (Color - Type)
+                            ForEach(2..<6) {
+                                Circle()    // Nutrition Color
+                                    .frame(width: 15, height: 15)
+                                    .foregroundColor(nutritionColor[$0])
+                                    .padding(.trailing, 4)
+                                
+                                Text(nutritionType[$0]) // Nutrition Type
+                                    .font(.system(size: 12))
+                                    .foregroundColor(colorGray)
+                                    .frame(minWidth: 32, alignment: .leading)
+                                    .padding(.trailing, 8)
+                            }
+                        }   // ~HStack(Nutrition Item 2/2)
+                        .padding(.leading, 15)
+                        .padding(.trailing, 52)
+                        
+                    }   // ~VStack
+                }   // ~ZStack(Chart)
+            }   // ~VStack
+            .ignoresSafeArea()
+            
+            HStack(spacing: 0) {    // Arrow - Sticker - Arrow
+                Image(systemName: "arrowtriangle.left.fill") // prev
+                    .resizable()
+                    .aspectRatio(CGSize(width: 0.75, height: 1), contentMode: .fit)
+                    .frame(width: 20, height: 15)
+                    .padding(.trailing, 60)
+                
+                Image(systemName: "face.smiling.inverse")    // Sticker
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80)
+                    .clipShape(Circle())
+                
+                Image(systemName: "arrowtriangle.right.fill") // next
+                    .resizable()
+                    .aspectRatio(CGSize(width: 0.75, height: 1), contentMode: .fit)
+                    .frame(width: 20, height: 15)
+                    .padding(.leading, 60)
+            }   // ~HStack
+            .padding(.top, 100)
+            .padding(.bottom, 664)
         }   // ~ZStack
         .ignoresSafeArea()
     }
