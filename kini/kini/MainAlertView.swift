2d27d2e6a0463239620a51c70116fe49d75350cc
zyeongPark <piiy10141997@gmail.com>
2023-06-12 17:08:40 +0900

    [ADD]: MainView 레이아웃

---
+++ kini/kini/MainAlertView.swift
@@ -0,0 +1,20 @@
+//
+//  MainAlertView.swift
+//  kini
+//
+//  Created by qwd on 2023/06/12.
+//
+
+import SwiftUI
+
+struct MainAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainAlertView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainAlertView()
+    }
+}
--- kini/kini/kiniApp.swift
+++ kini/kini/kiniApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct kiniApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
