f9d7c4edecf114fba0dda864a15856ed96b2e602
jaelyung <jaekim7724@gmail.com>
2023-06-14 00:23:20 +0900

    [ADD]: CoreData에 사용되는 DataModel 추가

--- kini/kini/ContentView.swift
+++ kini/kini/ContentView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjContext
+    @FetchRequest(sortDescriptors: []) var users: FetchedResults<User>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("contentView")
     }
+    
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
