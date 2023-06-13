529913b542d78c1a5e17145032cba5ffbdf187da
jaelyung <jaekim7724@gmail.com>
2023-06-14 01:38:39 +0900

    [ADD]: CoreData, Model 최초생성

---
+++ kini/kini/DataController.swift
@@ -0,0 +1,45 @@
+//
+//  DataController.swift
+//  kini
+//
+//  Created by jaelyung kim on 2023/06/13.
+//
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Model")
+
+    init() {
+        container.loadPersistentStores { desc, error in
+            if let error = error {
+                print("Fail to load the data \(error.localizedDescription)")
+            }
+        }
+    }
+
+    func save(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+            print("Data save!!! WUHU!!!")
+        } catch {
+            print("We could not save the data...")
+        }
+    }
+
+    func addGuardian(guardian: String, context:NSManagedObjectContext) {
+        let user = User(context: context)
+        user.guardian = guardian
+    }
+
+    func addUser(nickName: String, gender: String, age: String, guardian: String, context: NSManagedObjectContext) {
+        let user = User(context: context)
+        user.nickName = nickName
+        user.gender = gender
+        user.age = age
+        user.guardian = guardian
+
+        save(context: context)
+    }
+
+}
--- kini/kini/kiniApp.swift
+++ kini/kini/kiniApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct kiniApp: App {
+    @StateObject private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             MainView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
